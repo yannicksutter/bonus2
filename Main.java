7d151d0c46ef8e2571db11b7daaeb606df6563bd
Yannick Sutter <yannick.sutter@students.fhnw.ch>
2018-05-02 15:18:21 +0200

    md5, jdk changes

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class Main {
 
 
-    int l = 2000;
+    private static int l = 7;
     private static ArrayList<String> passwords = new ArrayList<String>();
     private static String init = "0000000";
     private static char[] chars = new char[]{'0', '1', '2', '3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x', 'y', 'z'};
@@ -17,6 +17,7 @@ public class Main {
 
         System.out.println("Hello World!");
         System.out.println(getMD5("0000000"));
+        System.out.println(getReduction(getMD5("0000000"), 0));
     }
 
 
@@ -31,10 +32,29 @@ public class Main {
             passwords.add(init);
         }
     }
-    
+
     private static String getMD5(String s) throws UnsupportedEncodingException, NoSuchAlgorithmException {
         byte[] bytesOfMessage = s.getBytes("UTF-8");
         MessageDigest md = MessageDigest.getInstance("MD5");
         return new BigInteger(1,md.digest(bytesOfMessage)).toString(16);
     }
+
+    private static String getReduction(String hash, int step) {
+        BigInteger bi = new BigInteger(hash, 16);
+        bi.add(BigInteger.valueOf(step));
+        BigInteger[] bis = new BigInteger[chars.length];
+        for(int i = 0; i < l; i++) {
+            bis[i] = bi.mod(BigInteger.valueOf(chars.length));
+            bi = bi.divide(BigInteger.valueOf(chars.length));
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        for(int x = bis.length-1; x >= 0; x--) {
+            sb.append(chars[bis[x].intValue()]);
+        }
+        return sb.toString();
+    }
+
+
 }
