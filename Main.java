45a861290b2bdb01433a28c94d40cc6a8ff9e34b
Yannick Sutter <yannick.sutter@students.fhnw.ch>
2018-05-02 13:39:07 +0200

    md5 function

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class Main {
@@ -9,29 +13,12 @@ public class Main {
     private static char[] chars = new char[]{'0', '1', '2', '3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x', 'y', 'z'};
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
 
         System.out.println("Hello World!");
-        for(int i = 7; i > 0; i--) {
-            fillPasswords(7);
-        }
-        for(String s : passwords) {
-            System.out.println(s);
-        }
+        System.out.println(getMD5("0000000"));
     }
 
-    private void createPasswords() {
-        String init = "0000000";
-        passwords.add(init);
-        for(int i = 0; i < 2000; i++) {
-            for(int j = 7; j > 0; j--){
-
-                for(char c : chars) {
-                    init.substring(0, j-1).concat(String.valueOf(c));
-                }
-            }
-        }
-    }
 
     private static void fillPasswords(int idx) {
         for(char c : chars) {
@@ -43,5 +30,11 @@ public class Main {
             init = init.substring(0, idx).concat(String.valueOf(c));
             passwords.add(init);
         }
+    }
+    
+    private static String getMD5(String s) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        byte[] bytesOfMessage = s.getBytes("UTF-8");
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        return new BigInteger(1,md.digest(bytesOfMessage)).toString(16);
     }
 }
